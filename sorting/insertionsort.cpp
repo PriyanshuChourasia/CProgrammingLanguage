6813f23c246a35a0084fed23f528783940426dd3
Priyanshu Chourasia <priyanshuchourasia916@gmail.com>
2025-05-31 16:19:11 +0530

    Insertion sort

---
+++ sorting/insertionsort.cpp
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+// insertion sort
+
+
+int main()
+{
+	int num;
+	printf("Enter size: ");
+	scanf("%d",&num);
+	
+	int arr[num];
+	
+	printf("Enter array: ");
+	for(int i=0; i<num; i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	
+	
+	printf("\n");
+	
+	for(int i=0; i<num; i++)
+	{
+		for(int j=i-1; j>=0; j--)
+		{
+			if(arr[j+1] > arr[j])
+			{
+				break;
+			}
+			else
+			{
+				int temp = arr[j+1];
+				arr[j+1] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+	
+	
+	printf("\n");
+	printf("Sorted array: \n");
+	
+	for(int i=0; i<num; i++)
+	{
+		printf("%d \n",arr[i]);
+	}
+	
+	return 0;
+}
