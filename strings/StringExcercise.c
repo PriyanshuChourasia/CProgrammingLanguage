3459a5ba2ebbbf7bf36196f6bab6094675e5cfe2
Priyanshu Chourasia <priyanshuchourasia916@gmail.com>
2025-07-16 11:27:20 +0530

    String file update

---
+++ strings/StringExcercise.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    /*/*Scanf does not take input after space its stop reading after space so we will use fgets#1#
+    char name[100];
+    printf("Enter your name: ");
+    fgets(name, sizeof(name),stdin);
+    printf("Your name is: %s", name);
+
+
+    char sentence[100];
+    printf("Enter any sentence: ");
+    fgets(sentence, sizeof(sentence), stdin);
+
+    printf("Size of %llu\n",sizeof(sentence));
+
+    printf("Sentence: %s", sentence);*/
+    /*------------------------------------------------------*/
+
+    /*Takes a string as input from the user.*/
+
+    /*
+    char name[100];
+    printf("Enter name: ");
+    fgets(name, sizeof(name), stdin);
+
+    printf("Name you entered: %s",name);
+    */
+
+    /*------------------------------------------------------------*/
+
+    /* Find Length of a String*/
+
+    /*
+    char name[100];
+    printf("Enter your name: ");
+    fgets(name,sizeof(name),stdin);
+
+    int cnt=0;
+    int i=0;
+    int totcount = 0;
+
+    while (name[i] != '\0') {
+        if (name[i] != ' ' && name[i] != '\0' && name[i] != '\n') {
+            cnt++;
+        }
+        totcount++;
+        i = i+1;
+    }
+
+    printf("\n Length of characters: %d\n",cnt);
+    printf("Name Entered: %s",name);
+    */
+/*----------------------------------------------------------------------------------*/
+
+    char name[100];
+    char name2[100];
+    printf("Enter your name: ");
+    fgets(name, sizeof(name),stdin);
+    strcpy(name2,name);
+
+    /*int i=0;
+
+    while (name[i] != '\0') {
+        name2[i] = name[i];
+        i = i+1;
+    }*/
+
+    printf("Copied name: %s",name2);
+
+
+    return 0;
+}
