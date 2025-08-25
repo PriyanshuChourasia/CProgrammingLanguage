c1cbb774e2c4fdd0670d8520a730c50f7e906f27
Priyanshu Chourasia <priyanshuchourasia916@gmail.com>
2025-08-25 16:49:26 +0530

    Big O topic

---
+++ TimeComplexity/TimeComplexityOne.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    /*
+     *So, the time complexity is constant O(1) every time a constant amount of time is required to execute code no matter on OS i am trying to run
+     * printf("Hello World");
+     */
+
+    /*
+     *The number will be printed n times as per condition of loops
+     *so the complexity is linear O(n) every time a linear amount of time is reuqired to execute the code
+     *
+     *int i, n = 8;
+        for (i=0; i<n; i++) {
+            printf("%d\n",i);
+        }
+    */
+
+    return 0;
+}
